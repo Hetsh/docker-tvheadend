21ffa81b97eecae8d9b6367265694fff06c6cad3
Hetsh <git@hetsh.de>
2025-10-13 20:15:51 +0200

    build.sh: restructure build options

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 
 # Abort on any error
-set -e -u
+set -e -u -o pipefail
 
 # Simpler git usage, relative file paths
 CWD=$(dirname "$0")
@@ -19,13 +19,51 @@ if ! docker version &> /dev/null; then
 	exit 1
 fi
 
+# Build image and export it's identifier to variable IMG_ID
+function build_image {
+	docker build .
+	IMG_ID=$(docker build -q .)
+}
+
+# Find tags for this image and export them to variable TAGS
+function find_tags {
+	TAGS=("latest")
+
+	# Account for git to be in detached head state
+	local GIT_BRANCH && GIT_BRANCH="$(git branch --show-current)"
+	if test -n "$GIT_BRANCH"; then
+		GIT_BRANCH=$(basename "$GIT_BRANCH")
+		TAGS+=("$GIT_BRANCH")
+	fi
+
+	# Account for multiple tags on the current commit
+	for GIT_TAG in $(git tag --points-at); do
+		GIT_TAG=$(basename "$GIT_TAG")
+		TAGS+=("$GIT_TAG")
+	done
+}
+
+# Tags the image with all tags that were found
+function apply_tags {
+	for TAG in "${TAGS[@]}"; do
+		TAGGED_IMAGE="$IMG_NAME:$TAG"
+		docker tag "$IMG_ID" "$TAGGED_IMAGE"
+		echo "Tagged image: $TAGGED_IMAGE"
+	done
+}
+
 IMG_NAME="hetsh/tvheadend"
-case "${1-}" in
-	# Build and test with default configuration
+TASK="${1-}"
+case "$TASK" in
+	# Build image and assign tags
+	"--tag")
+		build_image
+		find_tags
+		apply_tags
+	;;
+	# Build image and run test with default configuration
 	"--test")
-		docker build \
-			--tag "$IMG_NAME:test" \
-			.
+		build_image
 		docker run \
 			--rm \
 			--tty \
@@ -33,26 +71,32 @@ case "${1-}" in
 			--publish 9981:9981/tcp \
 			--publish 9982:9982/tcp \
 			--mount type=bind,source=/etc/localtime,target=/etc/localtime,readonly \
-			"$IMG_NAME:test"
+			"$IMG_ID"
 	;;
-	# Build if it does not exist and push image to docker hub
+	# Build and push tagged image
 	"--upload")
-		if ! tag_exists "$IMG_NAME"; then
-			docker build \
-				--tag "$IMG_NAME:latest" \
-				--tag "$IMG_NAME:$_NEXT_VERSION" \
-				.
-			docker push "$IMG_NAME:latest"
-			docker push "$IMG_NAME:$_NEXT_VERSION"
-			
-			# Remove version for easier image cleanup
-			docker image rm "$IMG_NAME:$_NEXT_VERSION"
+		if tag_exists "$IMG_NAME"; then
+			echo "Image already exists, no need to upload!"
+			exit 0
 		fi
+
+		build_image
+		find_tags
+		apply_tags
+		for TAG in "${TAGS[@]}"; do
+			docker push "$IMG_NAME:$TAG"
+		done
 	;;
-	# Build image without additonal steps
+	# Build image and output image identifier
+	"")
+		build_image
+		echo "Build successful!"
+		echo "The image has not been tagged!"
+		echo "Use the image ID instead: $IMG_ID"
+	;;
+	# Catch and notify about unknown task
 	*)
-		docker build \
-			--tag "$IMG_NAME:latest" \
-			.
+		echo_error "Unknown task \"$TASK\"!"
+		exit 1
 	;;
 esac
