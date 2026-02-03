ea90e0ea61454b6158d769dc12f13999bacc5633
Hetsh <git@hetsh.de>
2026-02-03 10:18:03 +0100

    Adjust to updated submodule scripts 4728a5e..1793f9d:   > update.sh: fix keyword in function update_base_image   > update.sh: minor changes   > update.sh: add missing return value UNSUPPORTED_PACKAGE_MANAGER   > update.sh: added function check_for_updates   > update.sh: detect used package manager in update_packages   > update.sh: replaced function update_image with smarter update_base_image   > helper.sh: added return code SUCCESS   > build.sh: added function tag_image

--- custom/update.sh
+++ custom/update.sh
@@ -4,7 +4,7 @@
 # This file will be sourced by scripts/update.sh to customize the update process
 
 
-MAIN_ITEM="tvheadend"
+IMG_NAME="hetsh/tvheadend"
 GIT_VERSION="$(git describe --tags --first-parent --abbrev=0)"
-update_image "amd64/alpine" "\\d{8}" "Alpine Linux"
-update_packages_apk "hetsh/tvheadend"
+BASE_IMAGE_VERSION_REGEX="\\d{8}"
+MAIN_ITEM="tvheadend"
